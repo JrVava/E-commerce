eed6d6980c7d1928c4f67b2041ffff4e1a7d2229
prafulbkamhasoft <prafulb@kamhasoft.com>
2023-05-17 17:49:50 +0530

    Add Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React,{useEffect, useState} from 'react'
 import Navbar from './Navbar';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Category from './components.js/Category';
+// import Category from './components.js/Category';
 import ProductByCategorySlug from './components.js/ProductByCategorySlug';
 import ProductDetails from './components.js/ProductDetails';
+import Home from './components.js/Home';
 
 function App() {
   return (
@@ -12,11 +13,9 @@ function App() {
           <Navbar />
           <hr />
           <Routes>
+          <Route path="/" element={<Home />} />
             <Route path="/category/:uuid" element={<ProductByCategorySlug />} />
             <Route path="/product-details/:uuid" element={<ProductDetails />} />
-            {/* <Route path="projects/:projectName" element={<ProjectDetails />} /> */}
-            {/* <Route path="/projects" element={<Projects />} />
-            <Route path="projects/:projectName" element={<ProjectDetails />} /> */}
           </Routes>
         </BrowserRouter>
       {/* <Navbar/> */}
---
+++ frontend/src/components.js/Home.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { api } from "../utils/apis";
+import Card from 'react-bootstrap/Card';
+import SimpleImageSlider from "react-simple-image-slider";
+import { Link } from "react-router-dom"
+export default function Home (){
+    const [rootId,setRootId] = useState();
+    const [products, setProducts] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const getCategories  = async () => {
+      await api.get('categories').then((response) => {
+            for(let i = 0; i < response.data.categories.length; i++) {
+                setRootId(response.data.categories[i].uuid)
+            }
+      });
+    }
+    const getCategoryProduct = async () => {
+        // console.log(rootId);
+        
+            await api.get(`category/${rootId}`).then((res) => {
+                setProducts(res.data.productDetails)
+                setLoading(false);
+            })
+        
+    }
+    useEffect(() => {
+            if(rootId !== undefined){
+                getCategoryProduct()
+            }
+        }, [rootId])
+    useEffect(()=>{
+      getCategories()
+    },[])
+    console.log(products);
+    return (
+        <div className="container">
+            {
+                loading ? "Loading Please Wait..." : 
+            
+           <div className="row" >
+                {
+                    products.product && (
+                        products.product.map((product, index) => {
+                            return (
+                                <div className="col-4" key={index}>
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Body >
+                                            <SimpleImageSlider
+                                                width={230}
+                                                height={250}
+                                                images={product.media}
+                                                slideDuration={0.5}
+                                                autoPlay={true}
+                                            />
+                                            <Card.Title>{product.name}</Card.Title>
+                                            <Card.Text dangerouslySetInnerHTML={{ __html: product.short_decription }}>
+                                            </Card.Text>
+                                            <Card.Subtitle className="mb-2 text-muted">${product.price}</Card.Subtitle>
+                                            <Link  to={`../product-details/${product.id}`} className="btn btn-primary">More details</Link>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            )
+                        })
+                    )
+                }
+
+            </div>
+            }
+        </div>
+    )
+}
